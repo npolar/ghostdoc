e242c0327dea84942a025954479114537c838ac1
RDuX <ruben.dens@gmail.com>
2015-03-09 12:50:06 +0100

    Added address, recursive mode flags. fixed small typoes. Defined main io channel

--- main/ghostdoc.go
+++ main/ghostdoc.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"flag"
 	"github.com/codegangsta/cli"
+	"os"
 )
 
 func main() {
@@ -15,10 +15,16 @@ func InitGhostDoc() {
 	ghostdoc.Version = "0.0.1"
 	ghostdoc.Usage = "Client used posting data to REST apis"
 	ghostdoc.Flags = ConfigureFlags()
+	ghostdoc.Action = ProcessDocs
+	ghostdoc.Run(os.Args)
 }
 
 func ConfigureFlags() []cli.Flag {
 	return []cli.Flag{
+		cli.StringFlag{
+			Name:  "address, a",
+			Usage: "Set url to write to",
+		},
 		cli.IntFlag{
 			Name:  "concurrency, c",
 			Value: 2,
@@ -26,23 +32,22 @@ func ConfigureFlags() []cli.Flag {
 		},
 		cli.StringFlag{
 			Name:  "delimiter, d",
-			Value: "i",
+			Value: ",",
 			Usage: "Set the demlimiter for working with csv data",
 		},
 		cli.StringFlag{
 			Name:  "format, f",
 			Value: "json",
-			Usage: "Set the input format to watch for [json|csv|txt].",
+			Usage: "Set the input format [json|csv|txt]",
 		},
 		cli.StringFlag{
-			Name:  "http-verb",
+			Name:  "http-verb, h",
 			Value: "POST",
 			Usage: "Set the http verb to use [POST|PUT]",
 		},
 		cli.StringFlag{
-			Name:  "key, k",
-			Value: "data",
-			Usage: "Specify the key to use for the data when wrapping",
+			Name:  "key-map, k",
+			Usage: "Sets mapping file to use to rename headers/keys. Format {\"oldkey\": \"newkey\"}",
 		},
 		cli.StringFlag{
 			Name:  "merge, m",
@@ -53,16 +58,46 @@ func ConfigureFlags() []cli.Flag {
 			Usage: "Set pattern file to extract filename info and inject it into the result",
 		},
 		cli.StringFlag{
+			Name:  "output-folder, o",
+			Usage: "Set dir to write output files",
+		},
+		cli.StringFlag{
+			Name:  "payload-key, p",
+			Value: "data",
+			Usage: "Specify the key to use for the payload when wrapping",
+		},
+		cli.StringFlag{
 			Name:  "uuid, u",
-			Usage: "Set namespaced uuid generation. uuid's will be injected with the 'id' key [full|wrap].",
+			Usage: "Set namespaced uuid generation. uuid's will be injected with the 'id' key [full|wrap]",
 		},
 		cli.StringFlag{
 			Name:  "wrapper, w",
-			Usage: "Define wrapper content for the data",
+			Usage: "Define wrapper a wrapper for the payload",
 		},
-		cli.StringFlag{
-			Name:  "remap, r",
-			Usage: "Sets mapping file to use to rename keys. Format {\"oldkey\": \"newkey\"}",
+		cli.BoolFlag{
+			Name:  "recusrive, r",
+			Usage: "Recursive read mode. also process sub-dirs",
 		},
 	}
+}
+
+func ProcessDocs(c *cli.Context) {
+	// Create a buffered interface channel
+	var dataChan = make(chan interface{}, c.Int("concurrency"))
+
+	// Setup a new parser and pass the cli context and the dataChannel
+	parser, err := NewParser(c, &dataChan)
+
+	// Start a reader loop that reads data as quickly as possible and processes it in parallel
+
+	// Parse all the input and dump the results
+	contents, err := parser.Parse()
+
+	// Start a writer loop that writes data as quickly as it comes available on the channel.
+	// Should never exceed more then the concurrency configured. If write is slow this might
+	// Increase memory consumption.
+
+	// Generate output and write it to the api
+	writer, err := NewWriter(c)
+	writer.Write(contents)
 }
