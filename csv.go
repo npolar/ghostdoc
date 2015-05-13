46d97a5b5bf9736d6de7134816aa92d78d2d1309
RDuX <ruben.dens@gmail.com>
2015-05-13 15:33:13 +0200

    added new csv parser command and a csv type handler

---
+++ csv.go
@@ -0,0 +1,31 @@
+package ghostdoc
+
+import (
+	"regexp"
+)
+
+const (
+	csvExtRegex = `(?i)^.+\.csv|tsv|txt`
+)
+
+type CsvHandler struct {
+	Delimiter string
+}
+
+func NewCsvHandler(delimiter string) *CsvHandler {
+	return &CsvHandler{
+		Delimiter: delimiter,
+	}
+}
+
+// rawInput does a lazy check for raw inline input and returns true if matches
+func (c *CsvHandler) rawInput(argument string) bool {
+	rawcsv := regexp.MustCompile(`^(.*)` + c.Delimiter + `(.*)$`)
+	return rawcsv.MatchString(argument)
+}
+
+// supportedFile returns true if the filename meets the requirements
+func (c *CsvHandler) supportedFile(filename string) bool {
+	csvFile := regexp.MustCompile(csvExtRegex)
+	return csvFile.MatchString(filename)
+}
---
+++ csv_parser.go
@@ -0,0 +1,131 @@
+package ghostdoc
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"github.com/npolar/ciface"
+	"log"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type CsvParser struct {
+	Cli         *cli.Context
+	DataChannel chan interface{}
+	WaitGroup   *sync.WaitGroup
+	*ArgumentHandler
+}
+
+// CsvCommand specifies the cli interface for the csv parser
+func CsvCommand() cli.Command {
+	return cli.Command{
+		Name:  "csv",
+		Usage: "Parse delimiter separated value files (csv, tsv, etc...)",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "delimiter, d",
+				Value: ",",
+				Usage: "Set the delimiter char",
+			},
+			cli.StringFlag{
+				Name:  "comment, c",
+				Value: "#",
+				Usage: "Set the comment char",
+			},
+			cli.StringFlag{
+				Name:  "header, hd",
+				Usage: "Configure data header. If not set the first data line will be used",
+			},
+			cli.IntFlag{
+				Name:  "skip, s",
+				Usage: "Specify the number of lines to skip before parsing",
+			},
+		},
+		Action: processCsv,
+	}
+}
+
+func newCsvParser(c *cli.Context, dc chan interface{}, wg *sync.WaitGroup) *CsvParser {
+	parser := &CsvParser{
+		Cli:         c,
+		DataChannel: dc,
+		WaitGroup:   wg,
+	}
+
+	// Configure the argument handler and give it a channel for the raw data
+	inputChan := make(chan []byte, c.GlobalInt("concurrency"))
+	parser.ArgumentHandler = NewArgumentHandler(c, inputChan)
+	// Customize the argument handler to relate to csv values
+	parser.TypeHandler = &CsvHandler{
+		Delimiter: c.String("delimiter"),
+	}
+
+	return parser
+}
+
+func processCsv(c *cli.Context) {
+	var csvChan = make(chan interface{})
+	wg := &sync.WaitGroup{}
+
+	csvParser := newCsvParser(c, csvChan, wg)
+	csvParser.parse()
+
+	writer := NewWriter(c, csvChan, wg)
+	if err := writer.Write(); err != nil {
+		log.Println(err.Error())
+	}
+	// Wait for all go routines to finish before exiting
+	wg.Wait()
+}
+
+func (csv *CsvParser) parse() {
+	if ok, err := csv.hasArgs(); ok {
+		csv.processArguments()
+
+		go func() {
+			for {
+				csv.parseToInterface(<-csv.RawChan)
+				csv.RawSync.Done()
+			}
+		}()
+
+		csv.RawSync.Wait()
+
+	} else {
+		fmt.Println(err)
+	}
+}
+
+func (csv *CsvParser) parseToInterface(raw []byte) {
+	cif := ciface.NewParser(raw)
+	cif.Skip = csv.Cli.Int("skip")
+
+	if header := csv.Cli.String("header"); header != "" {
+		cif.Header = strings.Split(csv.Cli.String("header"), ",")
+	}
+
+	delimiterRune, _, _, _ := strconv.UnquoteChar(csv.Cli.String("delimiter"), '"')
+	cif.Reader.Comma = delimiterRune
+
+	commentRune, _, _, _ := strconv.UnquoteChar(csv.Cli.String("comment"), '"')
+	cif.Reader.Comment = commentRune
+
+	docs, err := cif.Parse()
+
+	// push the docs onto the data channel
+	for _, doc := range docs {
+		//    doc, err = p.parseFileName(fname, doc)
+
+		csv.WaitGroup.Add(1)
+
+		go func(d interface{}) {
+			csv.DataChannel <- d
+		}(doc)
+
+	}
+
+	if err != nil {
+		log.Println("[Parsing error]", err)
+	}
+}
