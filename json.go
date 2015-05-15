b8fa0f493ca0f1edae64192f897da34ae04130e1
RDuX <ruben.dens@gmail.com>
2015-05-15 14:42:44 +0200

    added basic typechecking logic

--- json.go
+++ json.go
@@ -1,1 +1,27 @@
 package ghostdoc
+
+import (
+	"regexp"
+)
+
+const (
+	jsonFileRegex = `(?i)^.+\.json|geojson|topojson$`
+)
+
+type JsonHandler struct{}
+
+func NewJsonHandler() *JsonHandler {
+	return &JsonHandler{}
+}
+
+// rawInput does a lazy check for raw inline input and returns true if matches
+func (j *JsonHandler) rawInput(argument string) bool {
+	rawJson := regexp.MustCompile(`^\[|{\".+\":.+}|]$`)
+	return rawJson.MatchString(argument)
+}
+
+// supportedFile returns true if the filename meets the requirements
+func (j *JsonHandler) supportedFile(filename string) bool {
+	jsonFile := regexp.MustCompile(jsonFileRegex)
+	return jsonFile.MatchString(filename)
+}
