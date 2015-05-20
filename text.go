052cef459e4d3b586fb616cf0af43ff84020f3e8
RDuX <ruben.dens@gmail.com>
2015-05-20 10:33:24 +0200

    added text type checking

--- text.go
+++ text.go
@@ -1,1 +1,27 @@
 package ghostdoc
+
+import (
+	"regexp"
+)
+
+const (
+	textFileRegex = `(?i)^.+\.txt$`
+)
+
+type TextHandler struct{}
+
+func NewTextHandler() *TextHandler {
+	return &TextHandler{}
+}
+
+// rawInput does a lazy check for raw inline input and returns true if matches
+func (j *TextHandler) rawInput(argument string) bool {
+	rawText := regexp.MustCompile(`^\[|{\".+\":.+}|]$`)
+	return rawText.MatchString(argument)
+}
+
+// supportedFile returns true if the filename meets the requirements
+func (j *TextHandler) supportedFile(filename string) bool {
+	textFile := regexp.MustCompile(textFileRegex)
+	return textFile.MatchString(filename)
+}
